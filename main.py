c53b6e93d1cfee5afc60aab07f37640a41ed484a
mikakudev <mikakudev@gmail.com>
2025-01-11 12:59:40 +0300

    восстановил код и добавли картинку

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
+import pygame
+from pipes import Pipe
+from pipes import pipe_images
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+FPS = 60
+CELL_SIZE = 80
+GRID_ROWS = 6   # Количество строк
+GRID_COLS = 6   # Количество столбцов
 
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
+pygame.display.set_caption("Водопровводныые трубы")
+clock = pygame.time.Clock()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+grid = [[None for _ in range(6)] for _ in range(6)]
+grid[2][2] = Pipe(2 * CELL_SIZE, 2 * CELL_SIZE, "straight")
 
+def draw_grid():
+    for row in range(6):
+        for col in range(6):
+            rect = pygame.Rect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(screen, (200,200,200), rect, 1)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def draw_pipes():
+    for row in grid:
+        for pipe in row:
+            if pipe:
+                pipe.draw(screen)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def handle_click(pos):
+    col = pos[0] // CELL_SIZE
+    row = pos[1] // CELL_SIZE
+    if 0 <= row < 6 and 0 <= col < 6 and grid[row][col]:
+        grid[row][col].rotate()
+
+# ОСновной игроговой цикл
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = event.pos
+            col = mouse_x // CELL_SIZE
+            row = mouse_x // CELL_SIZE
+
+            if 0 <= GRID_ROWS and 0 <= col < GRID_COLS:
+                pipe = grid[row][col]
+                if pipe:
+                    pipe.rotate()
+
+    screen.fill((0,0,0))
+    draw_grid()
+    draw_pipes()
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
---
+++ pipes.py
@@ -0,0 +1,27 @@
+import pygame
+
+CELL_SIZE = 80
+
+pipe_images = {
+    "straight": pygame.image.load("assets/images/straight.png")
+}
+
+for key in pipe_images:
+    pipe_images[key] = pygame.transform.scale(pipe_images[key], (CELL_SIZE, CELL_SIZE))
+
+
+class Pipe:
+    def __init__(self, x, y, pipe_type, rotation=0):
+        self.x = x
+        self.y = y
+        self.type = pipe_type
+        self.rotation = rotation
+        self.base_image = pipe_images[self.type]
+
+    def draw(self, screen):
+        rotated_image = pygame.transform.rotate(self.base_image, self.rotation)
+        rect = rotated_image.get_rect(center=(self.x + CELL_SIZE // 2, self.y + CELL_SIZE // 2))
+        screen.blit(rotated_image, rect.topleft)
+
+    def rotate(self):
+        self.rotation = (self.rotation + 90) % 360
